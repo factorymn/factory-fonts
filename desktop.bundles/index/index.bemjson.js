8a6bfd554ecaf3b148f6aba3270ee5c3dc9b4156
voischev <voischev.ivan@ya.ru>
2014-07-29 23:17:37 +0400

    add index page

---
+++ desktop.bundles/index/index.bemjson.js
@@ -0,0 +1,16 @@
+({
+    block: 'page',
+    title: 'BEM-FONT Lib',
+    head: [
+        { elem: 'css', url: '_index.css' }
+    ],
+    scripts: [{ elem: 'js', url: '_index.js' }],
+    content: [
+       {
+           block: 'content',
+           content: [
+               'block content'
+           ]
+       }
+    ]
+})
