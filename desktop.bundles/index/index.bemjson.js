e68e20ee977377b2f8662b4c1bc30839f02b2918
voischev <voischev.ivan@ya.ru>
2014-07-30 01:04:01 +0400

    cs

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,14 +1,14 @@
 ({
-    block: 'page',
-    title: 'BEM-FONT Lib',
-    head: [
-        { elem: 'css', url: '_index.css' }
+    block : 'page',
+    title : 'BEM-FONT Lib',
+    head : [
+        { elem : 'css', url : '_index.css' }
     ],
-    scripts: [{ elem: 'js', url: '_index.js' }],
-    content: [
+    scripts : [{ elem : 'js', url : '_index.js' }],
+    content : [
        {
-           block: 'content',
-           content: [
+           block : 'content',
+           content : [
                'block content'
            ]
        }
